390399a2f577948d37677ba63e6b9d00e8d230c2
MrMarmelad-creator <aydetore02@gmail.com>
2025-02-14 10:11:15 +0500

    add Engine.php

---
+++ src/Engine.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Php\Project45\Engine;
+
+use function cli\prompt;
+use function cli\line;
+
+function games($gameRule, $generateRound)
+{
+    $correctAnswersCount = 0;
+
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    echo "$gameRule\n";
+
+    while ($correctAnswersCount < 3) {
+        [$question, $correctAnswer] = $generateRound();
+        echo "Question: $question\n";
+        echo "Your answer: ";
+        $userInput = trim(fgets(STDIN));
+
+        if ((string) $correctAnswer === $userInput) {
+            echo "Correct!\n";
+            $correctAnswersCount++;
+        } else {
+            echo "'$userInput' is wrong answer ;(. Correct answer was '$correctAnswer'.\n";
+            return;
+        }
+    }
+    if ($correctAnswersCount === 3) {
+        echo "Congratulations, $name\n";
+    }
+}
