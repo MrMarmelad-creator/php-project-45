587dea32a704aa6f0dcf893c15c4f98143cee214
MrMarmelad-creator <aydetore02@gmail.com>
2025-02-18 08:54:14 +0500

    Update Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -9,7 +9,7 @@ function games($gameRule, $generateRound)
 {
     $correctAnswersCount = 0;
 
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     echo "$gameRule\n";
